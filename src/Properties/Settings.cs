119cca43b1e0c9a944d4211d890d2bdabd355db8
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2021-04-14 17:14:03 -0400

    Add settings and upgrade path

---
+++ src/Properties/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace BackupAssistant.Properties
+{
+    internal sealed partial class Settings : ApplicationSettingsBase
+    {
+        void Handle_SettingsLoaded(object sender, SettingsLoadedEventArgs e)
+        {
+            try
+            {
+                if (this.UpgradeRequired)
+                {
+                    this.Upgrade();
+                    this.UpgradeRequired = false;
+                    this.Save();
+                }
+            }
+            catch
+            {
+                // The configuration file could not be parsed.
+            }
+        }
+    }
+}
