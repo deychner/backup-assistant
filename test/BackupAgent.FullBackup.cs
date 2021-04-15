e6ccddf0e263325828405a9ec17a57a1d97f304f
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2021-04-15 16:43:38 -0400

    Add test classes for upcoming work

---
+++ test/BackupAgent.FullBackup.cs
@@ -0,0 +1,22 @@
+﻿using BackupAssistant.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BackupAssistant.Tests.BackupAgent
+{
+    [TestClass]
+    public class FullBackup
+    {
+        private Mock<IBackupStarter> _mock;
+        private Core.BackupAgent _backupAgent;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mock = new Mock<IBackupStarter>(MockBehavior.Strict);
+            _mock.Setup(a => a.AddToLogEntry(It.IsAny<string>()));
+
+            _backupAgent = new Core.BackupAgent(_mock.Object);
+        }
+    }
+}
