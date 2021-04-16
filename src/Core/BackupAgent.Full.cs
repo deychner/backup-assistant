73dc45391d3d413e653d8f1e61d144a17330680a
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2021-04-16 09:35:00 -0400

    Stub out backup methods for full and incremental

--- src/Core/BackupAgent.Full.cs
+++ src/Core/BackupAgent.Full.cs
@@ -1,6 +1,12 @@
-﻿namespace BackupAssistant.Core
+﻿using System.Threading;
+
+namespace BackupAssistant.Core
 {
     public partial class BackupAgent
     {
+        public static void RunFullBackup()
+        {
+            Thread.Sleep(10000);
+        }
     }
 }
--- src/Core/BackupAgent.Incremental.cs
+++ src/Core/BackupAgent.Incremental.cs
@@ -1,6 +1,12 @@
-﻿namespace BackupAssistant.Core
+﻿using System.Threading;
+
+namespace BackupAssistant.Core
 {
     public partial class BackupAgent
     {
+        public static void RunIncrementalBackup()
+        {
+            Thread.Sleep(10000);
+        }
     }
 }
