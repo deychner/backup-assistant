914b6f43246d9f3a60c009e569f3dd02264713e5
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2022-04-01 22:03:28 -0400

    Add basic MVVM components

---
+++ src/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupAssistant.ViewModels
+{
+    internal class MainWindowViewModel : ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
