0cc6df5b53c910f820a863d7a9dea800142d7105
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2022-04-01 22:26:03 -0400

    Add filter icon asset binding

--- src/Models/MainWindowModel.cs
+++ src/Models/MainWindowModel.cs
@@ -1,10 +1,14 @@
-﻿namespace BackupAssistant.Models
+﻿using System.Collections.ObjectModel;
+
+namespace BackupAssistant.Models
 {
     internal class MainWindowModel
     {
+        public ObservableCollection<string> Filters { get; set; }
+        
         public MainWindowModel()
         {
-
+            this.Filters = new ObservableCollection<string>();
         }
     }
 }
--- src/ViewModels/MainWindowViewModel.cs
+++ src/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+﻿using BackupAssistant.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
 
 namespace BackupAssistant.ViewModels
 {
     internal class MainWindowViewModel : ObservableObject
     {
+        private MainWindowModel _model;
+
         public MainWindowViewModel()
         {
+            _model = new MainWindowModel();
+        }
 
+        public string FilterImageSource
+        {
+            get
+            {
+                if (_model.Filters.Count == 0)
+                {
+                    return "/assets/filter.png";
+                }
+                else
+                {
+                    return "/assets/filter_apply.png";
+                }
+            }
         }
     }
 }
