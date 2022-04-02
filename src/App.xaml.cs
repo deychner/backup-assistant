8fe11fb1e5100372af43e62f4099b6b0bb6291bd
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2022-04-01 21:47:08 -0400

    Initial commit

---
+++ src/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BackupAssistant
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public partial class App : Application
+    {
+    }
+}
