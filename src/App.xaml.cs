6404b6d3504b0c5443232563f785851730993fa7
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2022-04-01 22:03:52 -0400

    Automated code cleanup

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 
 namespace BackupAssistant
