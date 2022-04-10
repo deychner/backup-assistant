0028508538688b8190020cf402683ccbf08c6fb5
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2022-04-09 23:30:35 -0400

    Add messenger objects

---
+++ src/DataModels/FiltersChangedMessage.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+using System.Collections.ObjectModel;
+
+namespace BackupAssistant.DataModels
+{
+    internal sealed class FiltersChangedMessage : ValueChangedMessage<ObservableCollection<FilterItem>>
+    {
+        public FiltersChangedMessage(ObservableCollection<FilterItem> value) : base(value)
+        {
+
+        }
+    }
+}
