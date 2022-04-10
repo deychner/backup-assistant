302874af0d81218fee56c308c6a1113ffa1a5f2b
Darryl Eychner <79725061+deychner@users.noreply.github.com>
2022-04-09 23:29:55 -0400

    Add filter item list data model

---
+++ src/DataModels/FilterItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BackupAssistant.DataModels
+{
+    internal class FilterItem
+    {
+        public bool IsChecked { get; set; } = false;
+        public string Path { get; set; } = string.Empty;
+    }
+}
