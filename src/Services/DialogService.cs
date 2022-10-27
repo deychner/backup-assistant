745d5b4f83938a9324f12f3618fc2bcf86b781c9
Darryl E <79725061+deychner@users.noreply.github.com>
2022-10-27 15:54:45 -0400

    Add Dialog Service

---
+++ src/Services/DialogService.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace BackupAssistant.Services
+{
+    public class DialogService : IDialogService
+    {
+        public (DialogResult, string) ShowFolderBrowserDialog(string selectedPath = "")
+        {
+            using FolderBrowserDialog dialog = new()
+            {
+                SelectedPath = selectedPath
+            };
+
+            return (dialog.ShowDialog(), dialog.SelectedPath);
+        }
+    }
+}
---
+++ src/Services/IDialogService.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace BackupAssistant.Services
+{
+    public interface IDialogService
+    {
+        (DialogResult, string) ShowFolderBrowserDialog(string selectedPath = "");
+    }
+}
