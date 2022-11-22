fc09e53effcdd251a7f36cfa464731adb3c0623b
Darryl E <79725061+deychner@users.noreply.github.com>
2022-11-22 11:24:11 -0500

    Remove old messaging classes

--- src/DataModels/CurrentFiltersRequestMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
-using System.Collections.ObjectModel;
-
-namespace BackupAssistant.DataModels
-{
-    internal sealed class CurrentFiltersRequestMessage : RequestMessage<ObservableCollection<FilterItem>>
-    {
-    }
-}
