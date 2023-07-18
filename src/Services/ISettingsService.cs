a7fe9fb7fd583029ede33e95a732af1398faf896
Darryl E <darryl.eychner@blackbaud.com>
2023-07-18 09:56:33 -0400

    Add settings service

---
+++ src/Services/ISettingsService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Specialized;
+
+namespace BackupAssistant.Services
+{
+    public interface ISettingsService
+    {
+        int BackupType { get; set; }
+
+        string Destination { get; set; }
+
+        StringCollection Filters { get; set; }
+
+        string Source { get; set; }
+
+        // Intentionally disabled, since we do not want users changing this value
+        //bool UpgradeRequired { get; set; }
+
+        void Save();
+    }
+}
---
+++ src/Services/SettingsService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BackupAssistant.Services
+{
+    [ExcludeFromCodeCoverage]
+    public class SettingsService : ISettingsService
+    {
+        public int BackupType
+        {
+            get => Properties.Settings.Default.BackupType;
+            set { Properties.Settings.Default.BackupType = value; }
+        }
+
+        public string Destination
+        {
+            get => Properties.Settings.Default.Destination;
+            set { Properties.Settings.Default.Destination = value; }
+        }
+
+        public StringCollection Filters
+        {
+            get => Properties.Settings.Default.Filters;
+            set { Properties.Settings.Default.Filters = value; }
+        }
+
+        public string Source
+        {
+            get => Properties.Settings.Default.Source;
+            set { Properties.Settings.Default.Source = value; }
+        }
+
+        public void Save()
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
