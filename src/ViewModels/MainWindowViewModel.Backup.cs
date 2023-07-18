dd2c42b8064d281af5c87adfb6efd44ce804ff84
Darryl E <darryl.eychner@blackbaud.com>
2023-07-18 14:53:12 -0400

    Refactor Main Window ViewModel

---
+++ src/ViewModels/MainWindowViewModel.Backup.cs
@@ -0,0 +1,39 @@
+﻿using BackupAssistant.DataModels;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+
+namespace BackupAssistant.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        public IAsyncRelayCommand RunBackupCommand => new AsyncRelayCommand(RunBackup, CanRunBackup);
+
+        public BackupType BackupType
+        {
+            get => _model.BackupType;
+            set
+            {
+                _model.BackupType = value;
+                OnPropertyChanged(nameof(BackupType));
+
+                // Update settings
+                _settingsService.BackupType = (int)_model.BackupType;
+                _settingsService.Save();
+
+                // Update dependencies
+                OnPropertyChanged(nameof(RunBackupCommand));
+            }
+        }
+
+        public Task RunBackup()
+        {
+            return Task.Delay(1000);
+        }
+
+        public bool CanRunBackup()
+        {
+            return !string.IsNullOrEmpty(this.Source) && !string.IsNullOrEmpty(this.Destination);
+        }
+    }
+}
--- src/ViewModels/MainWindowViewModel.cs
+++ src/ViewModels/MainWindowViewModel.cs
@@ -2,115 +2,15 @@
 using BackupAssistant.Models;
 using BackupAssistant.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace BackupAssistant.ViewModels
 {
-    internal class MainWindowViewModel : ObservableObject
+    public partial class MainWindowViewModel : ObservableObject
     {
         private readonly MainWindowModel _model;
         private readonly ISettingsService _settingsService;
         private readonly IDialogService _dialogService;
-
-        public IRelayCommand AddEditSourceCommand => new RelayCommand(AddEditSource);
-        public IRelayCommand AddEditDestinationCommand => new RelayCommand(AddEditDestination);
-        public IRelayCommand EditFiltersCommand => new RelayCommand(() => EditFilters(new FilterSelectionViewModel()), CanEditFilters);
-        public IAsyncRelayCommand RunBackupCommand => new AsyncRelayCommand(RunBackup, CanRunBackup);
-
-        public string Source
-        {
-            get => _model.Source;
-            set
-            {
-                _model.Source = value;
-                OnPropertyChanged(nameof(Source));
-
-                // Update settings
-                _settingsService.Source = _model.Source;
-                _settingsService.Save();
-
-                // Update dependencies
-                if (!_model.Source.Equals(value))
-                {
-                    this.FilterItems = new ObservableCollection<string>();
-                }
-
-                OnPropertyChanged(nameof(EditFiltersCommand));
-                OnPropertyChanged(nameof(RunBackupCommand));
-            }
-        }
-
-        public string Destination
-        {
-            get => _model.Destination;
-            set
-            {
-                _model.Destination = value;
-                OnPropertyChanged(nameof(Destination));
-
-                // Update settings
-                _settingsService.Destination = _model.Destination;
-                _settingsService.Save();
-
-                // Update dependencies
-                OnPropertyChanged(nameof(RunBackupCommand));
-            }
-        }
-
-        public BackupType BackupType
-        {
-            get => _model.BackupType;
-            set
-            {
-                _model.BackupType = value;
-                OnPropertyChanged(nameof(BackupType));
-
-                // Update settings
-                _settingsService.BackupType = (int)_model.BackupType;
-                _settingsService.Save();
-
-                // Update dependencies
-                OnPropertyChanged(nameof(RunBackupCommand));
-            }
-        }
-
-        public ObservableCollection<string> FilterItems
-        {
-            get { return _model.Filters; }
-            set
-            {
-                _model.Filters = value;
-                OnPropertyChanged(nameof(FilterItems));
-
-                // Update settings
-                _settingsService.Filters.Clear();
-                _settingsService.Filters.AddRange(_model.Filters.ToArray<string>());
-                _settingsService.Save();
-
-                // Update dependencies
-                OnPropertyChanged(nameof(FilterImageSource));
-            }
-        }
-
-        public string FilterImageSource
-        {
-            get
-            {
-                if (_model.Filters.Count > 0)
-                {
-                    return "/assets/filter_apply.png";
-                }
-                else
-                {
-                    return "/assets/filter.png";
-                }
-            }
-        }
 
         public MainWindowViewModel(ISettingsService settingsService, IDialogService dialogService)
         {
@@ -138,53 +38,6 @@ namespace BackupAssistant.ViewModels
             this.Source = _settingsService.Source;
             this.Destination = _settingsService.Destination;
             this.BackupType = (BackupType)_settingsService.BackupType;
-        }
-
-        public void AddEditSource()
-        {
-            (DialogResult dialogResult, string selectedPath) = _dialogService.ShowFolderBrowserDialog(this.Source);
-
-            if (dialogResult == DialogResult.OK)
-            {
-                this.Source = selectedPath;
-            }
-        }
-
-        public void AddEditDestination()
-        {
-            (DialogResult dialogResult, string selectedPath) = _dialogService.ShowFolderBrowserDialog(this.Destination);
-
-            if (dialogResult == DialogResult.OK)
-            {
-                this.Destination = selectedPath;
-            }
-        }
-
-        public void EditFilters(IDialogViewModel dialogViewModel)
-        {
-            dialogViewModel.Input = new FilterSelectionInput { RootPath = this.Source, ExistingFilters = this.FilterItems };
-
-            bool? dialogResult = _dialogService.ShowDialog<FilterSelection>(dialogViewModel);
-
-            if (dialogResult.HasValue && dialogResult.Value)
-            {
-                this.FilterItems = (ObservableCollection<string>)dialogViewModel.Output;
-            }
-        }
-
-        public bool CanEditFilters()
-        {
-            return !string.IsNullOrEmpty(this.Source);
-        }
-
-        public Task RunBackup()
-        {
-            return Task.Delay(1000);
-        }
-
-        public bool CanRunBackup()
-        {
-            return !string.IsNullOrEmpty(this.Source) && !string.IsNullOrEmpty(this.Destination);
         }
 
         public int Progress
