a0889d1e611f090ae9fb7951c8a179c8b9b98869
Darryl E <darryl.eychner@blackbaud.com>
2023-07-19 10:39:11 -0400

    Add ability to cancel the run backup command

--- src/ViewModels/MainWindowViewModel.Backup.cs
+++ src/ViewModels/MainWindowViewModel.Backup.cs
@@ -1,13 +1,44 @@
 ﻿using BackupAssistant.DataModels;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BackupAssistant.ViewModels
 {
     public partial class MainWindowViewModel : ObservableObject
     {
-        public IAsyncRelayCommand RunBackupCommand => new AsyncRelayCommand(RunBackup, CanRunBackup);
+        private AsyncRelayCommand _runBackupCommand;
+        public IAsyncRelayCommand RunBackupCommand => _runBackupCommand ??= new AsyncRelayCommand(async (CancellationToken token) => await RunBackup(token), CanRunBackup);
+
+        public ICommand CancelRunBackupCommand => this.RunBackupCommand.CreateCancelCommand();
+
+        public async Task RunBackup(CancellationToken token)
+        {
+            try
+            {
+                await Task.Run(() => RunBackupInternal(token), token);
+            }
+            catch (OperationCanceledException)
+            {
+                // No action is needed
+            }
+        }
+
+        public bool CanRunBackup()
+        {
+            return !string.IsNullOrEmpty(this.Source) && !string.IsNullOrEmpty(this.Destination) && !_runBackupCommand.IsRunning;
+        }
+
+        internal void RunBackupInternal(CancellationToken token)
+        {
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+            }
+        }
 
         public BackupType BackupType
         {
@@ -24,16 +55,6 @@ namespace BackupAssistant.ViewModels
                 // Update dependencies
                 OnPropertyChanged(nameof(RunBackupCommand));
             }
-        }
-
-        public Task RunBackup()
-        {
-            return Task.Delay(1000);
-        }
-
-        public bool CanRunBackup()
-        {
-            return !string.IsNullOrEmpty(this.Source) && !string.IsNullOrEmpty(this.Destination);
         }
     }
 }
