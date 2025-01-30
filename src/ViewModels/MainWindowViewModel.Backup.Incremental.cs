4c2f229e2bb1bf9a28d709b1bb2cb510e4bb59cc
Darryl Eychner <darryl.eychner@blackbaud.com>
2025-01-30 15:12:53 -0500

    Add parallelization to file fetching for incremental backups

--- src/ViewModels/MainWindowViewModel.Backup.Incremental.cs
+++ src/ViewModels/MainWindowViewModel.Backup.Incremental.cs
@@ -1,5 +1,6 @@
 ﻿using BackupAssistant.DataModels;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace BackupAssistant.ViewModels
             // Get file list
             this.ProgressBarIsIndeterminate = true;
             this.Status = "Getting file information...";
-            IDictionary<string, FileListing> files = GetCombinedFileList(this.Source, this.Destination, token);
+            IDictionary<string, FileListing> files = await GetCombinedFileListAsync(this.Source, this.Destination, token);
 
             // Remove entries where BackupAction is None
             files = files
@@ -78,134 +79,144 @@ namespace BackupAssistant.ViewModels
             this.Status = "Backup is complete.";
         }
 
-        public IDictionary<string, FileListing> GetCombinedFileList(string sourceDirectory, string destinationDirectory, CancellationToken token)
+        public async Task<IDictionary<string, FileListing>> GetCombinedFileListAsync(string sourceDirectory, string destinationDirectory, CancellationToken token)
         {
-            Dictionary<string, FileListing> files = [];
+            ConcurrentDictionary<string, FileListing> files = new();
 
             if (this.FilterItems.Count > 0)
             {
                 // Get files in root source directory
                 this.Status = "Getting file information for source directory...";
-                CollectFilesForSourceListing(sourceDirectory, files);
+                CollectFilesForSourceListing(sourceDirectory, files, token);
 
                 // Get files in filtered source directories and all subdirectories
-                foreach (string f in this.FilterItems)
+                var sourceTasks = this.FilterItems.Select(async f =>
                 {
                     // Check for cancellation
                     token.ThrowIfCancellationRequested();
 
                     // Each filter directory is a relative path
                     string d = GetFullFileName(f, sourceDirectory);
-                    SourceDirectorySearch(d, files, token);
-                }
+                    await SourceDirectorySearchAsync(d, files, token);
+                });
+
+                await Task.WhenAll(sourceTasks);
 
                 // Get files in root destination directory
                 this.Status = "Getting file information for destination directory...";
-                CollectFilesForDestinationListing(destinationDirectory, files);
+                CollectFilesForDestinationListing(destinationDirectory, files, token);
 
-                // Get files in filtered source directories and all subdirectories
-                foreach (string f in this.FilterItems)
+                // Get files in filtered destination directories and all subdirectories
+                var destinationTasks = this.FilterItems.Select(async f =>
                 {
                     // Check for cancellation
                     token.ThrowIfCancellationRequested();
 
                     // Each filter directory is a relative path
                     string d = GetFullFileName(f, destinationDirectory);
-                    DestinationDirectorySearch(d, files, token);
-                }
+                    await DestinationDirectorySearchAsync(d, files, token);
+                });
+
+                await Task.WhenAll(destinationTasks);
             }
             else
             {
                 this.Status = "Getting file information for source directory...";
-                SourceDirectorySearch(sourceDirectory, files, token);
+                await SourceDirectorySearchAsync(sourceDirectory, files, token);
 
                 this.Status = "Getting file information for destination directory...";
-                DestinationDirectorySearch(destinationDirectory, files, token);
+                await DestinationDirectorySearchAsync(destinationDirectory, files, token);
             }
 
             return files;
         }
 
-        private void SourceDirectorySearch(string directory, IDictionary<string, FileListing> fileList, CancellationToken token)
+        private async Task SourceDirectorySearchAsync(string directory, ConcurrentDictionary<string, FileListing> fileList, CancellationToken token)
         {
-            CollectFilesForSourceListing(directory, fileList);
+            CollectFilesForSourceListing(directory, fileList, token);
 
-            foreach (string d in SafeGetDirectories(directory))
+            var tasks = SafeGetDirectories(directory).Select(async d =>
             {
                 // Check for cancellation
                 token.ThrowIfCancellationRequested();
 
-                SourceDirectorySearch(d, fileList, token);
-            }
+                await SourceDirectorySearchAsync(d, fileList, token);
+            });
+
+            await Task.WhenAll(tasks);
         }
 
-        private void CollectFilesForSourceListing(string directory, IDictionary<string, FileListing> fileList)
+        private void CollectFilesForSourceListing(string directory, ConcurrentDictionary<string, FileListing> fileList, CancellationToken token)
         {
-            foreach (string f in SafeGetFiles(directory))
+            var files = SafeGetFiles(directory);
+
+            foreach (var f in files)
             {
+                // Check for cancellation
+                token.ThrowIfCancellationRequested();
+
                 string fileName = ShrinkSourceFileName(f);
                 IFileInfo? info = SafeGetFileInfo(f);
 
                 if (info != null)
                 {
-                    if (fileList.TryGetValue(fileName, out FileListing? existingListing))
+                    fileList.AddOrUpdate(fileName, new FileListing
+                    {
+                        IsInSource = true,
+                        SourceLastModified = info.LastWriteTime,
+                        Size = info.Length
+                    },
+                    (key, existingListing) =>
                     {
                         existingListing.IsInSource = true;
                         existingListing.SourceLastModified = info.LastWriteTime;
-                    }
-                    else
-                    {
-                        FileListing listing = new()
-                        {
-                            IsInSource = true,
-                            SourceLastModified = info.LastWriteTime,
-                            Size = info.Length
-                        };
-
-                        fileList.Add(fileName, listing);
-                    }
+                        return existingListing;
+                    });
                 }
             }
         }
 
-        private void DestinationDirectorySearch(string directory, IDictionary<string, FileListing> fileList, CancellationToken token)
+        private async Task DestinationDirectorySearchAsync(string directory, ConcurrentDictionary<string, FileListing> fileList, CancellationToken token)
         {
-            CollectFilesForDestinationListing(directory, fileList);
+            CollectFilesForDestinationListing(directory, fileList, token);
 
-            foreach (string d in SafeGetDirectories(directory))
+            var tasks = SafeGetDirectories(directory).Select(async d =>
             {
                 // Check for cancellation
                 token.ThrowIfCancellationRequested();
 
-                DestinationDirectorySearch(d, fileList, token);
-            }
+                await DestinationDirectorySearchAsync(d, fileList, token);
+            });
+
+            await Task.WhenAll(tasks);
         }
 
-        private void CollectFilesForDestinationListing(string directory, IDictionary<string, FileListing> fileList)
+        private void CollectFilesForDestinationListing(string directory, ConcurrentDictionary<string, FileListing> fileList, CancellationToken token)
         {
-            foreach (string f in SafeGetFiles(directory))
+            var files = SafeGetFiles(directory);
+
+            foreach (var f in files)
             {
+                // Check for cancellation
+                token.ThrowIfCancellationRequested();
+
                 string fileName = ShrinkDestinationFileName(f);
                 IFileInfo? info = SafeGetFileInfo(f);
 
                 if (info != null)
                 {
-                    if (fileList.TryGetValue(fileName, out FileListing? existingListing))
+                    fileList.AddOrUpdate(fileName, new FileListing
+                    {
+                        IsInDestination = true,
+                        DestinationLastModified = info.LastWriteTime,
+                        Size = info.Length
+                    },
+                    (key, existingListing) =>
                     {
                         existingListing.IsInDestination = true;
                         existingListing.DestinationLastModified = info.LastWriteTime;
-                    }
-                    else
-                    {
-                        FileListing listing = new()
-                        {
-                            IsInDestination = true,
-                            DestinationLastModified = info.LastWriteTime,
-                            Size = info.Length
-                        };
-
-                        fileList.Add(fileName, listing);
-                    }
+                        return existingListing;
+                    });
                 }
             }
         }
