1cd598930cd2a7e80660245594c7abe4ef48772b
Darryl Eychner <darryl.eychner@icloud.com>
2026-07-02 23:08:34 -0700

    Configure logging

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using BackupAssistant.Services;
 using BackupAssistant.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Windows;
 
 namespace BackupAssistant
@@ -45,7 +48,31 @@ namespace BackupAssistant
             // View models
             _ = services.AddTransient<MainWindowViewModel>();
 
+            // Logging
+            ConfigureLogging(services);
+
             return services.BuildServiceProvider();
+        }
+
+        private static void ConfigureLogging(ServiceCollection services)
+        {
+            var logPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Anaheim_Electronics", "logs", "backup-.log");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(logPath,
+                    rollingInterval: RollingInterval.Hour,
+                    retainedFileCountLimit: 5,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+
+            services.AddLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.AddSerilog(dispose: true);
+            });
         }
     }
 }
